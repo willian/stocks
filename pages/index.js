8d3f07cb29e442b6d22c297b5417d4e523f4c443
Willian Fernandes <willian@willianfernandes.com.br>
2020-11-24 09:01:13 -0300

    Install tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div>
-      Hello World.
+      <div className="py-20">
+        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
+          Hello World
+        </h1>
+      </div>
     </div>
   )
 }
