9af9cfdcd6674e0a7c2c95a736dc0728eb1a88bb
Willian Fernandes <willian@willianfernandes.com.br>
2020-11-24 18:38:47 -0300

    Remove extra spaces from top and bottom

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <div className="py-20">
         <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
           Hello World
         </h1>
-      </div>
     </div>
   )
 }
