e33b36a5aa206599025adbf22e8a93e304e87f27
Willian Fernandes <willian@willianfernandes.com.br>
2020-11-27 06:48:26 -0300

    Add a service worker

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
 import Head from 'next/head'
 import { Provider } from 'next-auth/client'
+import { useEffect } from 'react'
 
 import '../styles.css'
 
 import Header from '../components/Header'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(
+          function (registration) {
+            console.log('Service Worker registration successful with scope: ', registration.scope)
+          },
+          function (err) {
+            console.log('Service Worker registration failed: ', err)
+          },
+        )
+      })
+    }
+  }, [])
+
   return (
     <Provider options={{ site: process.env.SITE }} session={pageProps.session}>
       <Head>
