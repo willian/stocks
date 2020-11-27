08703e80a915a52a1b12972a4d68b23cf5a13c45
Willian Fernandes <willian@willianfernandes.com.br>
2020-11-27 10:24:57 -0300

    Use the global fetcher to filte companies

--- components/Holdings/AsyncCompanySelect.js
+++ components/Holdings/AsyncCompanySelect.js
@@ -1,31 +1,15 @@
 import debounce from 'lodash.debounce'
 import { forwardRef } from 'react'
-import { request, gql } from 'graphql-request'
+import fetcher from '../../utils/fetcher'
+
+import { FILTER_COMPANIES } from '../../graphql/queries'
 
 import AsyncSelect from '../forms/AsyncSelect'
 
-const fetcher = (query, variables) =>
-  request('https://mechanism-stocks.herokuapp.com/v1/graphql', query, variables)
-
 const fetchCompanyOptions = (str, callback) => {
-  const query = gql`
-    query filterCompanies($query: String = "") {
-      companies(
-        where: { _or: [{ name: { _ilike: $query } }, { symbol: { _ilike: $query } }] }
-        limit: 100
-        order_by: { symbol: asc }
-      ) {
-        id
-        name
-        industry
-        sector
-        symbol
-      }
-    }
-  `
   const variables = { query: `%${str}%` }
 
-  fetcher(query, variables).then(({ companies }) => {
+  fetcher(FILTER_COMPANIES, variables).then(({ companies }) => {
     const options = companies.map((c) => ({ label: `${c.symbol}: ${c.name}`, value: c.id }))
     callback(options)
   })
--- graphql/queries.js
+++ graphql/queries.js
@@ -1,5 +1,21 @@
 import { gql } from 'graphql-request'
 
+export const FILTER_COMPANIES = gql`
+  query filterCompanies($query: String = "") {
+    companies(
+      where: { _or: [{ name: { _ilike: $query } }, { symbol: { _ilike: $query } }] }
+      limit: 100
+      order_by: { symbol: asc }
+    ) {
+      id
+      name
+      industry
+      sector
+      symbol
+    }
+  }
+`
+
 export const MY_HOLDINGS = gql`
   query MyHoldings {
     holdings {
