db5468bd1c0982e69f04928b29d3ffb381a617b8
Willian Fernandes <willian@willianfernandes.com.br>
2020-11-26 22:06:23 -0300

    Fix the Grid component

--- components/Grid.js
+++ components/Grid.js
@@ -1,13 +1,13 @@
 function Col({ children, span: spanProp, smSpan: smSpanProp }) {
-  const span = `col-span-${spanProp}`
-  const smSpan = `sm:col-span-${smSpanProp}`
+  const span = spanProp ? `col-span-${spanProp}` : ''
+  const smSpan = smSpanProp ? `sm:col-span-${smSpanProp}` : ''
 
   return <div className={`${span} ${smSpan}`}>{children}</div>
 }
 
 export default function Grid({ cols: colsProp, gap: gapProp, children }) {
-  const cols = `grid-cols-${colsProp}`
-  const gap = `gap-${gapProp}`
+  const cols = colsProp ? `grid-cols-${colsProp}` : ''
+  const gap = gapProp ? `gap-${gapProp}` : ''
 
   return <div className={`grid ${cols} ${gap} `}>{children}</div>
 }
