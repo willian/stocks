ab29c46177f48f9d33035638f44dd7b0f62413ea
Willian Fernandes <willian@willianfernandes.com.br>
2020-11-27 07:13:56 -0300

    Configure next-pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 const withPWA = require('next-pwa')
 
-module.exports = withPWA({})
+module.exports =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : withPWA({
+        pwa: {
+          dest: 'public',
+        },
+      })
